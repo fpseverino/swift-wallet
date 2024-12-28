3804ece7eb2c39aec97319e07f3b64eaeea1340f
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-12-28 16:36:30 +0100

    Improve testing

--- Tests/WalletOrdersTests/WalletOrdersTests.swift
+++ Tests/WalletOrdersTests/WalletOrdersTests.swift
@@ -1,9 +1,18 @@
+import Crypto
 import Foundation
 import Testing
 import WalletOrders
+import Zip
 
 @Suite("WalletOrders Tests")
 struct WalletOrdersTests {
+    let decoder = JSONDecoder()
+    let order = TestOrder()
+
+    init() {
+        Zip.addCustomFileExtension("order")
+    }
+
     @Test("Build Order")
     func build() throws {
         let builder = OrderBuilder(
@@ -13,11 +22,11 @@ struct WalletOrdersTests {
         )
 
         let bundle = try builder.build(
-            order: TestOrder(),
+            order: order,
             sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletOrdersTests/SourceFiles"
         )
 
-        #expect(bundle != nil)
+        try testRoundTripped(bundle)
     }
 
     @Test("Build Order with Encrypted Key")
@@ -30,11 +39,11 @@ struct WalletOrdersTests {
         )
 
         let bundle = try builder.build(
-            order: TestOrder(),
+            order: order,
             sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletOrdersTests/SourceFiles"
         )
 
-        #expect(bundle != nil)
+        try testRoundTripped(bundle)
     }
 
     @Test("Build Pass without Source Files")
@@ -47,7 +56,7 @@ struct WalletOrdersTests {
 
         #expect(throws: WalletOrdersError.noSourceFiles) {
             try builder.build(
-                order: TestOrder(),
+                order: order,
                 sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletOrdersTests/NoSourceFiles"
             )
         }
@@ -65,9 +74,34 @@ struct WalletOrdersTests {
 
         #expect(throws: WalletOrdersError.noOpenSSLExecutable) {
             try builder.build(
-                order: TestOrder(),
+                order: order,
                 sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletOrdersTests/SourceFiles"
             )
         }
+    }
+
+    private func testRoundTripped(_ bundle: Data) throws {
+        let orderURL = FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID().uuidString).order")
+        try bundle.write(to: orderURL)
+        let orderFolder = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString, isDirectory: true)
+        try Zip.unzipFile(orderURL, destination: orderFolder)
+
+        #expect(FileManager.default.fileExists(atPath: orderFolder.path.appending("/signature")))
+
+        #expect(FileManager.default.fileExists(atPath: orderFolder.path.appending("/pet_store_logo.png")))
+        #expect(FileManager.default.fileExists(atPath: orderFolder.path.appending("/it-IT.lproj/pet_store_logo.png")))
+
+        #expect(FileManager.default.fileExists(atPath: orderFolder.path.appending("/order.json")))
+        let orderData = try String(contentsOfFile: orderFolder.path.appending("/order.json")).data(using: .utf8)
+        let roundTrippedOrder = try decoder.decode(TestOrder.self, from: orderData!)
+        #expect(roundTrippedOrder.authenticationToken == order.authenticationToken)
+        #expect(roundTrippedOrder.orderIdentifier == order.orderIdentifier)
+
+        let manifestJSONData = try String(contentsOfFile: orderFolder.path.appending("/manifest.json")).data(using: .utf8)
+        let manifestJSON = try decoder.decode([String: String].self, from: manifestJSONData!)
+        let iconData = try Data(contentsOf: orderFolder.appendingPathComponent("/icon.png"))
+        #expect(manifestJSON["icon.png"] == SHA256.hash(data: iconData).map { "0\(String($0, radix: 16))".suffix(2) }.joined())
+        #expect(manifestJSON["pet_store_logo.png"] != nil)
+        #expect(manifestJSON["it-IT.lproj/pet_store_logo.png"] != nil)
     }
 }
--- Tests/WalletPassesTests/WalletPassesTests.swift
+++ Tests/WalletPassesTests/WalletPassesTests.swift
@@ -1,9 +1,18 @@
+import Crypto
 import Foundation
 import Testing
 import WalletPasses
+import Zip
 
 @Suite("WalletPasses Tests")
 struct WalletPassesTests {
+    let decoder = JSONDecoder()
+    let pass = TestPass()
+
+    init() {
+        Zip.addCustomFileExtension("pkpass")
+    }
+
     @Test("Build Pass")
     func build() throws {
         let builder = PassBuilder(
@@ -13,11 +22,11 @@ struct WalletPassesTests {
         )
 
         let bundle = try builder.build(
-            pass: TestPass(),
+            pass: pass,
             sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletPassesTests/SourceFiles"
         )
 
-        #expect(bundle != nil)
+        try testRoundTripped(bundle)
     }
 
     @Test("Build Pass with Encrypted Key")
@@ -30,11 +39,11 @@ struct WalletPassesTests {
         )
 
         let bundle = try builder.build(
-            pass: TestPass(),
+            pass: pass,
             sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletPassesTests/SourceFiles"
         )
 
-        #expect(bundle != nil)
+        try testRoundTripped(bundle)
     }
 
     @Test("Build Pass with Personalization")
@@ -54,12 +63,12 @@ struct WalletPassesTests {
         )
 
         let bundle = try builder.build(
-            pass: TestPass(),
+            pass: pass,
             sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletPassesTests/SourceFiles",
             personalization: testPersonalization
         )
 
-        #expect(bundle != nil)
+        try testRoundTripped(bundle, with: testPersonalization)
     }
 
     @Test("Build Pass without Source Files")
@@ -72,7 +81,7 @@ struct WalletPassesTests {
 
         #expect(throws: WalletPassesError.noSourceFiles) {
             try builder.build(
-                pass: TestPass(),
+                pass: pass,
                 sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletPassesTests/NoSourceFiles"
             )
         }
@@ -90,9 +99,45 @@ struct WalletPassesTests {
 
         #expect(throws: WalletPassesError.noOpenSSLExecutable) {
             try builder.build(
-                pass: TestPass(),
+                pass: pass,
                 sourceFilesDirectoryPath: "\(FileManager.default.currentDirectoryPath)/Tests/WalletPassesTests/SourceFiles"
             )
         }
+    }
+
+    private func testRoundTripped(_ bundle: Data, with personalization: PersonalizationJSON? = nil) throws {
+        let passURL = FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID().uuidString).pkpass")
+        try bundle.write(to: passURL)
+        let passFolder = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString, isDirectory: true)
+        try Zip.unzipFile(passURL, destination: passFolder)
+
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/signature")))
+
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/logo.png")))
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/personalizationLogo.png")))
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/it-IT.lproj/logo.png")))
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/it-IT.lproj/personalizationLogo.png")))
+
+        #expect(FileManager.default.fileExists(atPath: passFolder.path.appending("/pass.json")))
+        let passData = try String(contentsOfFile: passFolder.path.appending("/pass.json")).data(using: .utf8)!
+        let roundTrippedPass = try decoder.decode(TestPass.self, from: passData)
+        #expect(roundTrippedPass.authenticationToken == pass.authenticationToken)
+        #expect(roundTrippedPass.serialNumber == pass.serialNumber)
+        #expect(roundTrippedPass.description == pass.description)
+
+        if let personalization {
+            let personalizationJSONData = try String(contentsOfFile: passFolder.path.appending("/personalization.json")).data(using: .utf8)
+            let personalizationJSON = try decoder.decode(PersonalizationJSON.self, from: personalizationJSONData!)
+            #expect(personalizationJSON.description == personalization.description)
+        }
+
+        let manifestJSONData = try String(contentsOfFile: passFolder.path.appending("/manifest.json")).data(using: .utf8)!
+        let manifestJSON = try decoder.decode([String: String].self, from: manifestJSONData)
+        let iconData = try Data(contentsOf: passFolder.appendingPathComponent("/icon.png"))
+        #expect(manifestJSON["icon.png"] == Insecure.SHA1.hash(data: iconData).map { "0\(String($0, radix: 16))".suffix(2) }.joined())
+        #expect(manifestJSON["logo.png"] != nil)
+        #expect(manifestJSON["personalizationLogo.png"] != nil)
+        #expect(manifestJSON["it-IT.lproj/logo.png"] != nil)
+        #expect(manifestJSON["it-IT.lproj/personalizationLogo.png"] != nil)
     }
 }
