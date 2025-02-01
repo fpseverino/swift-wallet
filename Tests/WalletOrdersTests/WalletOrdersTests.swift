f691d7d7971cbed98e6426537bcf30858f423ef1
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-02-01 15:44:27 +0100

    Add OpenSSL installation step in Windows CI

--- Tests/WalletOrdersTests/WalletOrdersTests.swift
+++ Tests/WalletOrdersTests/WalletOrdersTests.swift
@@ -10,7 +10,7 @@ struct WalletOrdersTests {
     let order = TestOrder()
 
     #if os(Windows)
-        let openSSLPath = #"C:\Program Files\Git\usr\bin\openssl.exe"#
+        let openSSLPath = #"C:\ProgramData\chocolatey\lib\openssl"#
     #else
         let openSSLPath: String = "/usr/bin/openssl"
     #endif
--- Tests/WalletPassesTests/WalletPassesTests.swift
+++ Tests/WalletPassesTests/WalletPassesTests.swift
@@ -10,7 +10,7 @@ struct WalletPassesTests {
     let pass = TestPass()
 
     #if os(Windows)
-        let openSSLPath = #"C:\Program Files\Git\usr\bin\openssl.exe"#
+        let openSSLPath = #"C:\ProgramData\chocolatey\lib\openssl"#
     #else
         let openSSLPath: String = "/usr/bin/openssl"
     #endif
